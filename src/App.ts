4b6a003b6a0334094c7f9015178c2b72b2d9129a
Keita Okamoto <ok@aaa.email.ne.jp>
2017-10-31 05:37:23 +0900

    Scaffold

---
+++ src/App.ts
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default Vue.extend({
+  name: 'app',
+  components: {
+    HelloWorld,
+  },
+})
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script lang="ts" src="./App.ts"></script>
+<style src="./App.scss"></style>
