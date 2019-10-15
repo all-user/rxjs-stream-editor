2e979661510065215ba682e12044c58271e7d7de
Keita Okamoto <ooorue@gmail.com>
2019-10-15 18:45:47 +0900

    chore: Add submodule

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 import VueTextareaAutosize from 'vue-textarea-autosize';
+import { modASomeFunction } from '@all-user/mod-a';
+import { modBSomeFunction } from '@all-user/mod-b';
 
 Vue.use(VueTextareaAutosize);
 Vue.config.productionTip = false;
@@ -10,3 +12,6 @@ new Vue({
   store,
   render: h => h(App),
 }).$mount('#app');
+
+console.log(modASomeFunction());
+console.log(modBSomeFunction());
